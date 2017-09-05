4bba814344b4f561fb6dedb36e9264219e8eb601
Dan Kiss and Keegan Ruebling <dan@>
2017-09-05 16:21:08 -0700

    add functionality to leetspeak

---
+++ lib/leetspeak.rb
@@ -0,0 +1,34 @@
+class String
+  def leetspeak()
+    letters = []
+    letters = self.split("")
+    i = -1
+    while (i <= letters.length-1)
+      i += 1
+      if letters[i] === "e"
+        letters[i] = 3
+      end
+      if letters[i] === "o"
+        letters[i] = 0
+      end
+      if letters[i] === "I"
+        letters[i] = 1
+      end
+      if (letters[i] === "s") & (letters[i-1] === " ")
+        letters[i] = "s"
+      elsif letters[i] === "s"
+        letters[i] = "z"
+      end
+    end
+    return letters.join()
+  end
+end
+
+
+
+
+#
+# if (letters[i] === " ") & (letters[i+1] === "s")
+#   letters[i+1] = "s"
+# else letters[i] === "s"
+#     letters[i] = "z"
